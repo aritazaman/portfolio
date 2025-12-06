f128017e9de36537112ea6d5d7ccb81cebbc2237
Arita Z. <zaman.arita@gmail.com>
2025-12-06 23:21:01 +0000

    contact page, fixed darkmode toggle

--- darkmode.js
+++ darkmode.js
@@ -2,18 +2,22 @@ let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
+    document.documentElement.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.documentElement.classList.remove('darkmode')
+    localStorage.setItem('darkmode', 'inactive')
 }
 
-if (darkmode === "active") enableDarkmode()
+if (darkmode === "active") {
+    enableDarkmode()
+} else {
+    disableDarkmode()
+}
 
-themeSwitch.addEventListener("click", ()=> {
+themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
